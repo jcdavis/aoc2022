563fcac1f41f3063683e0b99e852e00db20ce982
Jackson Davis <jackson@jcdav.is>
2022-12-02 07:44:51 -0800

    d2p2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -9,11 +9,16 @@ fn main() {
   let result: i32 = lines.iter().map(|line| {
     let chars: Vec<char> = line.chars().collect();
     let them = (chars[0] as i32) - ('A' as i32);
-    let us = (chars[2] as i32) - ('X' as i32);
+    let us = match chars[2] { //(chars[2] as i32) - ('X' as i32);
+      'X' => (them + 2) % 3,
+      'Y' => them,
+      'Z' => (them + 1) % 3,
+      _ => panic!("wtf"),
+    };
     let win_score = match (us - them + 3) % 3 {
       0 => 3, //Tie
       1 => 6, //Win
-      2 => 0, //Draw
+      2 => 0, //Lose
       _ => panic!("wtf"),
     };
     win_score + us+1
