c06bbf7ba619395eb095b897f5196f762b253883
Jackson Davis <jackson@jcdav.is>
2022-12-07 20:22:13 -0800

    d4p1

---
+++ day4/src/main.rs
@@ -0,0 +1,22 @@
+use std::env;
+use std::fs;
+
+use regex::Regex;
+
+fn main() {
+  let args: Vec<String> = env::args().collect();
+  let input = fs::read_to_string(&args[1]).unwrap();
+  let re = Regex::new(r"(\d+)-(\d+),(\d+)-(\d+)").unwrap();
+
+  let total = input.lines().map(|line| {
+    let c = re.captures(line).unwrap();
+    ((c[1].parse::<i32>().unwrap(), c[2].parse::<i32>().unwrap()),
+      (c[3].parse::<i32>().unwrap(), c[4].parse::<i32>().unwrap()))
+  }).filter( |tuples| {
+      let ((left_min, left_max), (right_min, right_max)) = tuples;
+      (left_min <= right_min && left_max >= right_max) ||
+        (right_min <= left_min && right_max >= left_max)
+  }).count();
+
+  println!("{}", total);
+}
