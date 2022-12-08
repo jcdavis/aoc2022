008e6924356d1e75860f29d083d5b9647e6de176
Jackson Davis <jackson@jcdav.is>
2022-12-07 21:30:13 -0800

    d6p2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -7,16 +7,16 @@ fn main() {
   let input: Vec<char> = fs::read_to_string(&args[1]).unwrap().chars().collect();
   let mut buffer = Vec::new();
 
-  for i in 0..4 {
+  for i in 0..14 {
     buffer.push(input[i]);
   }
 
   let mut pos: usize = 0;
-  for i in 4..input.len() {
+  for i in 14..input.len() {
     buffer[pos] = input[i];
-    pos = (pos+1)%4;
+    pos = (pos+1)%14;
 
-    if buffer.iter().collect::<std::collections::HashSet<&char>>().len() == 4 {
+    if buffer.iter().collect::<std::collections::HashSet<&char>>().len() == 14 {
       println!("{} {:?}", i, buffer);
       return;
     }
